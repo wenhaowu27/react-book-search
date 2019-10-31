84ee77c94f256dcd8871872557d6d06b384b6b4b
Wenhao Wu <wenhaow@hotmail.com>
2019-10-31 12:35:22 -0700

    remove gitignore

---
+++ client/build/precache-manifest.af8e0f88b85bbdb275dca321a4325350.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa101868c5452356fec1",
+    "url": "/static/js/main.422347cc.chunk.js"
+  },
+  {
+    "revision": "301d9b67e070ea4f0e8a",
+    "url": "/static/js/2.0788af42.chunk.js"
+  },
+  {
+    "revision": "fa101868c5452356fec1",
+    "url": "/static/css/main.b08f6c42.chunk.css"
+  },
+  {
+    "revision": "89ee8fbbb3982efdec43eb5cadb8ff77",
+    "url": "/index.html"
+  }
+];
